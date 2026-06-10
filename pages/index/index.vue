c14eca82f86f6ce3b755feb6b53aa09647563bdf
Maluxiye <1289040549@qq.com>
2026-06-11 00:31:33 +0800

    吴彦锴来修改了

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">王家欣是大傻子</text>
 		</view>
 	</view>
 </template>
